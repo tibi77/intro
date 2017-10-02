a2cff0f08adfef874d3da53813e591e8a98c379a
tiberiu_lepadatu <tiberiulepadatu@yahoo.com>
2017-10-02 17:23:47 +0300

    first commit

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "student" | sudo -S apt install sl
+echo "student" | sudo -S apt install fortune 
+echo "student" | sudo -S apt install xcowsay
+echo "student" | sudo -S apt install toilet
+echo "student" | sudo -S apt install cmatrix
+echo "student" | sudo -S apt install libaa-bin
+
+
+
---
+++ use.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+while true; do echo "$(date '+%D %T' | toilet -f term -F border --gay)"; sleep 1; done
+
+
+
+##aafire
