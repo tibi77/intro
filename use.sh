92aa6a7e7d9eee9c673fee565206c491ea213e9c
tiberiu_lepadatu <tiberiulepadatu@yahoo.com>
2017-10-02 17:29:23 +0300

    2nd commit

--- use.sh
+++ use.sh
@@ -3,8 +3,9 @@
 
 while true; do echo "$(date '+%D %T' | toilet -f term -F border --gay)"; sleep 1; done
 sleep (10)
-cowsay -f gnu
 
 
 
 ##aafire
+##asciiview elephant.jpg -driver curses 
+##cowsay -f gnu
